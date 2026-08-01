2d8c8560554851b3ed865f770e3cf5c48bcdbf94
APMaii <ali.pilehvarmeibody@studenti.polito.it>
2026-08-01 18:22:43 +0330

    teh first code in class

---
+++ L1_2.py
@@ -0,0 +1,603 @@
+'''
+In The name of god
+
+name : Ali Pilehvar Meibody
+
+date =  2 mordad 1405 
+
+Class : fanaavri co 
+
+
+
+
+Dostan ...
+
+
+
+
+Human (En) <----- Interface(Python) --> Machine (Binary 0,1)
+
+
+
+dastoor bedam aaz human b machien --> programming 
+
+interface --> programming language ---> 
+
+mokhafaf karde yekseri code haro , dastoroate binary ro
+
+
+man baraye dashtane python
+
+
+1- Python kernel --> Run mikone
+2- Editor (vs code, sublime), IDE (spyder, pycharm) 
+
+
+Anaconda --> Environment haye mokhtalef sakht
+dakhelesh python mojaza , editore mojaza sakhtim
+
+
+
+
+
+Spyder hastim --> .py sakjhtim in file e ma hast
+
+kole spyder 4 bakhshe , ye ghesmate oon tool haye bala
+
+1- Tool balaye (new file, save file, import file, run, run section, setting)
+
+2- editor --> codeto minevsii
+
+3- samte rast yek ghesmati , variuables,plot , files , ...
+
+
+4- Console -->  run miseh ,javabe run ro mide
+
+
+
+run --> file .py mifrste be kernele Python
+
+Mesle yek ensan az bala b paeen az chap b rast mikhone
+
+ag error bod hamonja motevaghef mishe va barmigarde
+
+run shod t atahesh tamame jhavab ro pas mide (console)
+
+
+
+
+
+
+
+Kheyli barname hasasie
+bayad kalamat rol doros benevisi
+case sensetive (Print, PRINT ,print ,) print_ prinnt 
+
+
+2 ghesmate kole python
+
+1- Python built in functions
+Tavabeye dakheli python --> narenji mishan man mishnasam
+
+yek kari mikonan ,. print() namayesh midad
+input() 
+open()
+len()
+type() 
+
+
+
+2- Keywords -> banafsh --> logic 
+if else ,for while , def , ....
+
+
+harchi joz in benevsi -->sefid minevise
+
+
+3- 
+man sefid haro (harchizi joz python built in function (narenji),
+                keywords(banafhs ) , man in haro sefid mikonam
+                va mikham esme zarf bashe)
+
+
+
+Adad, kalame, harchiz ro zakhire koni va tarifesh koni 
+
+bayad berizi jaee bename Variable (moteghayer) [ZARF]
+
+zarf [ meghdar]
+
+variable [value] 
+
+zarf --> esm dare 
+
+10 ta zarf bkham .
+
+
+
+
+esme variable (zarf) ghavanin dare
+
+esmesho nmitoni az hamon python built in function keyword
+case sensetive --> A a 
+az hich characteri bejoz _ estefade koni
+nmitoni avalesh adad bzar
+
+
+daskhelesh value mziari 
+
+
+
+'''
+
+
+
+
+
+
+
+"""
+
+"""
+
+
+# salam dostan vaght bekheyr
+#
+#
+#
+#
+#
+
+print('hello world')
+
+
+len('ali') #Out[11]: 3
+
+
+type('ali') #Out[12]: str
+
+
+
+if True:
+    print('salam')
+
+
+
+
+
+#name -->esme zarfame 
+#10 meghdarame
+#10 ro rikhtm too uy zarf bename name
+name = 10
+
+
+
+a = 10
+b = 20
+c = 40 
+
+c = 50
+
+
+d = 70
+
+
+
+
+Print = 50
+
+#ghalat
+kelase*barname = 
+
+#ghalat
+kelase barname  =
+
+#ghhalat
+kelase-barname
+
+
+kelasebarname = 
+
+KelaseBarname = 
+
+kelase_barname = 
+
+Kelase_Barname =  
+
+
+2kelase_barname =
+
+kelase_barname2= 
+
+
+
+a = 10 
+
+ghade_ali = 10
+
+vazne_doost = 50
+
+vazn = 8000
+
+ghad = 600
+
+
+print(10) #10
+
+
+print(vazn)  #8000
+
+
+#zarf haro esm gozari krdi , k ssa koni
+#harja define , call later
+print(ghad) #600
+
+
+
+
+
+
+
+#-----------------------------------------
+'''
+Varibale haye motefavet darim
+
+Adad, kalame, boolean, iterables ,.....
+
+
+
+
+1- Python built in functions -> yek kari mikone, andaze, type , print,...
+
+2- Keywordeds ---> 
+
+3- Variables --> ESme zarf hastan
+
+
+zarf esme [value]
+
+
+variable values -->
+    1- Numbers (int ,float , complex) 
+    2- Boolean (True , False)
+    3-
+    
+
+
+'''
+
+a = 10 
+
+
+print(a) #10
+type(a) #Out[26]: int --> integer
+
+b= 10.7633555
+
+print(b) #10.7633555
+type(b) # float
+
+
+c = 1j
+print(c)
+type(c) #complex
+#i + aj  1+4j
+
+
+#define kardan (sakhtan)
+a= 10 
+b = 3.35
+
+
+#OPERATION riazi amaliate riazi
+#=
+#tavan , zarb , taghsim , jam , tafazol 
+
+
+a=10
+b=40
+
+#a + b = c
+#a + b = c
+
+
+c = a + b 
+
+print(c) #50
+
+
+c = a**2
+print(c) #100
+
+c = a * b  #400
+
+c = a / b #0.25
+
+c = a + b #50
+
+c = a - b #-30
+
+#dastoor midam a ra b b --> c
+
+#olaviat () > Tavan > zarb,tafrigh > jam ,tafazol
+
+b = a**2+5
+
+b = (a**2) + 5
+
+b = a**(2+5)
+ 
+#dastoor midaham
+
+#---Comparisonal operation
+#amaliat hay emoghayese
+#soal soal miporsam
+#==  !=  > >= < <=
+
+
+
+a=10
+
+b=40
+
+print(a>b) #False
+print(a>=b) #false
+
+
+print(a<b) #True
+print(a<=b) #True
+
+
+print(a==b) #False
+print(a!=b) #True
+
+c = a>b
+
+print(c) #False
+
+type(c) #bool
+
+#True , False
+
+d = True
+
+e = False
+
+
+print(ghad)
+
+#----------
+#esme zarf --> name
+#value --> ali
+
+#name = ali
+
+#string reshte
+
+name = 'ali'
+
+#zarfi besaz bename name
+#ali ro beriz dakhelesh
+
+print(name) #ali
+
+type(name) #str --> string
+
+
+#name = ali
+
+
+
+
+name = 50 
+ali = 60 
+
+name = ali 
+
+
+
+name = 'ali'
+
+brand = 'nike'
+
+#zarfi drm esme zarf brande, toosh chie kalamey nike e 
+
+print(brand) #nike
+
+
+
+
+z=10
+print(z)
+
+
+
+
+
+a= 10
+d=10
+
+print(a==d) #True
+
+
+
+
+
+
+a = 10
+b = '10'
+
+print(a==b) #False
+
+
+z = a/20
+print(z) #).5
+
+z = b /20
+#TypeError: unsupported operand type(s) for /: 'str' and 'int'
+
+
+
+
+
+print(type(a)) #<class 'int'>
+print(type(b)) #<class 'str'>
+
+
+name='ali'
+
+
+2*name # 'aliali'
+
+name/4384783747 #TypeError: unsupported operand type(s) for /: 'str' and 'int'
+
+
+'''
+
+Human <---Python--->Machine
+
+Python -->zaban (vocab, grammar)
+
+1---Python built in function (tavabe dakheliee python -->nareni)
+2--- Keywords (in logic haro miad if , else -->banafsh)
+3---->variable name --> esme zarf hast
+
+    value 
+    3.1. Numbers
+        3.1.1. Int 10,5,23,-1,-2,0
+        3.1.2. float 10.2332 , 103.323
+        3.1.3. complex  1+3j
+        
+        mathematical operations 
+        c = a ** b 
+        c = a * b
+        c = a / b
+        c = a + b
+        c = a - b 
+        
+        comparisonal operations
+        a == b
+        a != b
+        a > b
+        a >= b 
+        a < b 
+        a <= b 
+        
+        True False midadan , soal porsidi 
+        
+        
+    3.2. Booleans
+        a= True   , b = False
+       
+        
+       
+    3.3. String str --> reshte --> reshte ee az character haye keyworde laptobe shoam
+    
+        a = 'harchi delet miokhad benevis'
+        
+        name = 'ali'
+        brand = 'nike'
+        madrese = 'khayam'
+        danehsgah = 'tehran'
+        city = 'tehran'
+        
+        sen = '40' 4 0 --> matehmatcial operatiosn anjam nmishe
+        '40' != 40
+    
+    
+    
+        
+
+
+
+
+'''
+
+
+
+a='ali'
+
+b= "ali"
+
+print(a==b) #True
+
+
+
+sentence =' in the name of god'
+
+sentence2 = 'in the name of 'god''
+
+sentence2 = 'in the name of "god" '
+sentence2 = "in the name of 'god' "
+
+#ij chra felan shod ??
+
+
+
+
+
+
+
+'''
+
+1- yek dor film ra moshahede konid
+2- file e .py e man ro say konid roosh hamaro yekabr shode run konid, comment bzrid
+3- (optional) --> yek file folder l1.py -->kholase
+4- taklife 
+
+yek --> .py misazid  operation.py 
+4 ta adad misazid , tamame amaliat haye riazi ro rosh anjam midid
+
+do --> yek file .py comparison.py 
+4 ta adad msiazid, tamame amaliat haye moghayese ee roosh anjam mididd
+
+se --> se ta variabl misazid string bashand
+
+va len , type , print mikonideshoon
+
+
+3 ta .py ro azaton migirm --> email baraye man konid 
+
+ai.course22.alipilehvar@gmail.com
+
+Subject --> l1_q_name_last_name
+
+l1_q_mohammad_mohammadi --> academic
+
+QQ_mohammad_mohammadi
+
+
+l1 
+q quiz 
+ 
+
+
+
+
+
+
+
+
+
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
